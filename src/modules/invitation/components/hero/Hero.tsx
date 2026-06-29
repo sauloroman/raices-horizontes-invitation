28c9f9c66f9084f98d5cb220217a67123600be7d
Saulo Román Santillán Nava <romansantillan1998@outlook.com>
2026-06-28 22:12:04 -0600

    refactor(hero) ♻️: Update Hero section to use video background

--- src/modules/invitation/components/hero/Hero.tsx
+++ src/modules/invitation/components/hero/Hero.tsx
@@ -1,48 +1,18 @@
 import React from 'react'
-import { StarIcon } from '@phosphor-icons/react'
 
 import './_hero.scss'
-import hero from '@/assets/images/hero.png'
-import { Timer } from '../../../../common/components/Timer/Timer'
-
-const TARGET_DATE = import.meta.env.VITE_INVITATION_DATE
+import hero from '@/assets/videos/hero-1.mp4'
 
 export const Hero: React.FC = () => {
     return (
         <div className="hero">
-
-            <div className="hero__background" style={{ backgroundImage: `url(${hero})` }}></div>
-            <div className="hero__overlay"></div>
-
-            <div className="hero__container">
-                <div className="hero__top">
-                    <div className='hero__top-star'><StarIcon size={16} weight="fill" /></div>
-                    <span className='hero__top-celebration'>¡Nos Graduamos!</span>
-                </div>
-
-                <div className="hero__line"></div>
-
-                <div className="hero__date">
-                    <span className='hero__date-day'>05</span>
-                    <div className="hero__date-line"></div>
-                    <span className='hero__date-month'>Jul</span>
-                    <div className="hero__date-line"></div>
-                    <span className='hero__date-year'>26</span>
-                </div>
-
-                <div className="hero__title">
-                    <span className='hero__title-first'>Licenciatura</span> <br />
-                    <span className='hero__title-second'>Turismo</span>
-                </div>
-
+            <video className='hero__video' src={hero} autoPlay muted loop ></video>
+            <div className="hero__content">
+                <span className='hero__message'>¡Nos Graduamos!</span>
+                <h1 className='hero__title'>Enfermería</h1>
+                <p className='hero__date'>Julio 10, 2026</p>
                 <div className="hero__divider"></div>
-
-                <div className="hero__timer">
-                    <Timer targetDate={TARGET_DATE} />
-                </div>
             </div>
-
-
         </div>
     )
 }
