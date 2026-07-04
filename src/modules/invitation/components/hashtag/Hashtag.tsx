eacc48ec37b42b4eef5f28f076f7d03c9459079a
Saulo Román Santillán Nava <romansantillan1998@outlook.com>
2026-07-04 14:51:59 -0600

    refactor (hashtag) ♻️: actualizar imports de la camara y pulir estilos del hashtag

--- src/modules/invitation/components/hashtag/Hashtag.tsx
+++ src/modules/invitation/components/hashtag/Hashtag.tsx
@@ -3,12 +3,16 @@ import './_hashtag.scss'
 import { SectionHeader } from '@/common/components/SectionHeader/SectionHeader'
 import { InstagramLogoIcon } from '@phosphor-icons/react'
 import palmera from '@/assets/images/palmera.png'
+import camara from '@/assets/images/camara.png'
 
 export const Hashtag: React.FC = () => {
     return (
         <section className='hashtag'>
             <div className="hashtag__palmera">
                 <img src={palmera} alt="Palmera decorativa" />
+            </div>
+            <div className="hashtag__camara">
+                <img src={camara} alt="Camara fotográfica decorativa" />
             </div>
             <div className='hashtag__container'>
                 <div className="hashtag__header">
