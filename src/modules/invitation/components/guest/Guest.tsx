bf65a3a20d81e1db2baa37be8b8bdafef00fc6db
Saulo Román Santillán Nava <romansantillan1998@outlook.com>
2026-07-03 20:31:07 -0600

    refactor(guest) ♻️: Redesign Guest layout using frame image and SectionHeader

--- src/modules/invitation/components/guest/Guest.tsx
+++ src/modules/invitation/components/guest/Guest.tsx
@@ -1,29 +1,36 @@
 import React from 'react'
 import './_guest.scss'
-import { GraduationCapIcon } from '@phosphor-icons/react'
-import icon from '@/assets/images/guest-icon.png'
+import frame from '@/assets/images/guest-frame.jpeg'
+import { SectionHeader } from '@/common/components/SectionHeader/SectionHeader'
 
 export const Guest: React.FC = () => {
     return (
         <section className='guest'>
             <div className="guest__container">
+                <img className='guest__frame' src={frame} alt="frame" />
+
                 <div className="guest__content">
 
-                    <GraduationCapIcon className='guest__icon' />
-                    <div className="guest__title">Esta Invitación es para tí</div>
-                    <div className="guest__divider"></div>
+                    <div className="guest__header">
+                        <SectionHeader
+                            subtitle='Para tí'
+                            title='Invitación'
+                        />
+                    </div>
 
-                    <span className='guest__honor'>Invitado de honor</span>
-                    <p className="guest__name">Saulo Román Santillán Nava</p>
+                    <div className="guest__name">
+                        <p>Invitado Especial</p>
+                        <span>Saulo Román Santillán Nava</span>
+                    </div>
 
-                    <p className="guest__message">Es un honor para nosotros invitarte a ser parte de este momento tan especial en nuestras vidas</p>
-                    <div className="guest__button">
+                    <p className="guest__message">
+                        Eres el graduado mas esperado de esta generacion, este es tu gran día. Vívelo al máximo.
+                    </p>
+
+                    <div className='guest__button'>
                         <button>Ver Boletos</button>
                     </div>
 
-                    <div className="guest__icon-box">
-                        <img src={icon} alt="Icono de ramo de flores" />
-                    </div>
                 </div>
             </div>
         </section>
