6b8dbe31693bfd6c354515e963cd555a5d2cbf7e
Saulo Román Santillán Nava <romansantillan1998@outlook.com>
2026-07-03 20:31:14 -0600

    refactor(message) ♻️: Simplify Message layouts and add table icon element

--- src/modules/invitation/components/message/Message.tsx
+++ src/modules/invitation/components/message/Message.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import './_message.scss'
-import divider from '@/assets/images/divider-1.png'
+
+import table from '@/assets/images/table-icon.jpeg'
 
 export const Message: React.FC = () => {
     return (
         <section className="message">
-            <div className="message__container container">
-                <div className="message__line"></div>
+            <div className="message__container">
                 <p className='message__main'>Con la bendición de nuestros mentores y familia, damos el siguiente paso hacia un mundo de nuevos horizontes.</p>
                 <p className='message__text'>Acompáñanos en una noche de elegancia y celebración para marcar la culminación de nuestro camino académico.</p>
-                <img className='message__divider' src={divider} alt="Divider" />
             </div>
+            <img className='message__table' src={table} alt="table" />
         </section>
     )
 }
